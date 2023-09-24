aa83a30afc8b6a1b23948970756a7c45080ef36f
Alexander Pavlov <pavlov-promo@yandex.ru>
2023-09-24 22:04:03 +0300

    work project

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+def write_to_file(text):
+    with open("text.txt", "a") as file:
+        file.write(text + "\n")
+
+# Функция для чтения текста из файла
+def read_from_file():
+    try:
+        with open("text.txt", "r") as file:
+            content = file.readlines()
+        return content
+    except FileNotFoundError:
+        return []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        text = request.form["text"]
+        if text:
+            write_to_file(text)
+    content = read_from_file()
+    return render_template("index.html", content=content)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
